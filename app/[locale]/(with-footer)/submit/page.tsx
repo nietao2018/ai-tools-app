a72ee9d3288d6cd929966ea95f63985677c2827c
nietao <nietao2018@gmail.com>
2024-10-08 22:37:31 +0800

    feat: uifix

--- app/[locale]/(with-footer)/submit/page.tsx
+++ app/[locale]/(with-footer)/submit/page.tsx
@@ -23,11 +23,11 @@ export default function Page() {
 
   return (
     <div className='mx-auto max-w-pc'>
-      <div className='flex-y-center my-3 flex lg:my-10'>
-        <h1 className='text-5xl font-bold'>{t('title')}</h1>
-        <h2 className='mt-[5px] text-sm font-bold lg:my-3'>{t('subTitle')}</h2>
-        <SubmitForm />
+      <div className='flex-y-center mx-4 my-3 flex lg:my-10'>
+        <h1 className='text-center text-5xl font-bold'>{t('title')}</h1>
+        <h2 className='mt-[5px] text-center text-sm font-bold lg:my-3'>{t('subTitle')}</h2>
       </div>
+      <SubmitForm />
       <Faq />
     </div>
   );
